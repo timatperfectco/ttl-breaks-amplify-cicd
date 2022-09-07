d708b0c515de55a4817491a67e51aa2de6f92ff3
Tim Heilman <tdh@perfectco.com>
2022-09-07 13:34:02 -0700

    build: wipe-out repo to restart from steps
    b/c human error I messed things up.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "ttlbreaksamplifycicd": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
